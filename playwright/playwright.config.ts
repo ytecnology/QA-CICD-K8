7155c550af567d6814238f99c853c9b74acb00fe
ajytriag <armando.ytriago@cencosud.cl>
2023-11-21 16:52:51 -0300

    teser

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -67,7 +67,7 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
+ 
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
